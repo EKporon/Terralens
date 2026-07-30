3087ededab2c43a17148775cb1a1773cb749f66b
E.Kporon <127246453+EKporon@users.noreply.github.com>
2026-07-30 19:27:27 +0300

    Update validators.py

--- terralens/services/validators.py
+++ terralens/services/validators.py
@@ -1,1 +1,30 @@
+"""
+Input validation utilities.
+"""
 
+
+def validate_coordinates(
+    latitude,
+    longitude
+):
+
+
+    if not latitude or not longitude:
+
+        return False
+
+
+
+    if latitude < -90 or latitude > 90:
+
+        return False
+
+
+
+    if longitude < -180 or longitude > 180:
+
+        return False
+
+
+
+    return True
