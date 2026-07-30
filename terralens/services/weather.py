f28c08e4c7a02babd65e3569d71dcf776720040e
E.Kporon <127246453+EKporon@users.noreply.github.com>
2026-07-30 19:26:19 +0300

    Update weather.py

--- terralens/services/weather.py
+++ terralens/services/weather.py
@@ -1,1 +1,75 @@
+"""
+TerraLens Weather Service
 
+Gets current weather data.
+"""
+
+
+import requests
+
+from config import Config
+
+
+
+def get_weather(latitude, longitude):
+
+
+    params = {
+
+
+        "latitude":
+        latitude,
+
+
+        "longitude":
+        longitude,
+
+
+        "current":
+        "temperature_2m,relative_humidity_2m,wind_speed_10m"
+
+
+    }
+
+
+
+    response = requests.get(
+
+        Config.WEATHER_URL,
+
+        params=params
+
+    )
+
+
+
+    data = response.json()
+
+
+
+    if "current" not in data:
+
+        return None
+
+
+
+    current = data["current"]
+
+
+
+    return {
+
+
+        "temperature":
+        current["temperature_2m"],
+
+
+        "humidity":
+        current["relative_humidity_2m"],
+
+
+        "wind_speed":
+        current["wind_speed_10m"]
+
+
+    }
