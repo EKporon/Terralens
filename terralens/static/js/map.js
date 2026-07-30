e68361e6e3d853cf45dce6eb94e7e56c1f298781
E.Kporon <127246453+EKporon@users.noreply.github.com>
2026-07-30 19:12:42 +0300

    Implement map initialization and marker addition

--- terralens/static/js/map.js
+++ terralens/static/js/map.js
@@ -1,1 +1,80 @@
+/*
+=================================
+TerraLens Map Controller
+=================================
+*/
 
+
+let map;
+
+
+
+function initializeMap(){
+
+
+    map = L.map(
+        "map"
+    ).setView(
+        [
+            -1.9441,
+            30.0619
+        ],
+        13
+    );
+
+
+
+    L.tileLayer(
+
+        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+
+        {
+
+            attribution:
+            "© OpenStreetMap contributors"
+
+        }
+
+    ).addTo(map);
+
+
+
+    console.log(
+        "Map loaded"
+    );
+
+
+}
+
+
+
+
+
+function addMarker(
+    latitude,
+    longitude
+){
+
+
+    L.marker(
+        [
+            latitude,
+            longitude
+        ]
+    )
+    .addTo(map);
+
+
+    map.setView(
+
+        [
+            latitude,
+            longitude
+        ],
+
+        15
+
+    );
+
+
+}
