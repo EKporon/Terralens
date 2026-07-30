e82c6703a8aaa1bc1cd021903a94fd8dc02454d6
E.Kporon <127246453+EKporon@users.noreply.github.com>
2026-07-30 19:34:04 +0300

    Update app.py

--- terralens/app.py
+++ terralens/app.py
@@ -1,34 +1,66 @@
 from flask import Flask
 
+
 from config import Config
+
+
+from routes import (
+    home_bp,
+    api_bp,
+    errors_bp
+)
+
 
 
 def create_app():
 
+
     app = Flask(__name__)
 
-    app.config.from_object(Config)
+
+    app.config.from_object(
+        Config
+    )
 
 
-    @app.route("/")
-    def home():
+    app.register_blueprint(
+        home_bp
+    )
 
-        return """
-        <h1>TerraLens 🌍</h1>
-        <p>
-        Know the Land Before You Buy It.
-        </p>
-        """
+
+    app.register_blueprint(
+        api_bp
+    )
+
+
+    app.register_blueprint(
+        errors_bp
+    )
+
 
 
     return app
 
 
 
+
+
 if __name__ == "__main__":
+
 
     app = create_app()
 
+
+
+    app.run(
+
+        host="0.0.0.0",
+
+        port=5000,
+
+        debug=True
+
+    )
     app.run(
         host="0.0.0.0",
         port=5000,
