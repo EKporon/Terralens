8df09a2004eb85ecf514bbebab83170a59144cf8
E.Kporon <127246453+EKporon@users.noreply.github.com>
2026-07-30 19:26:43 +0300

    Update elevation.py

--- terralens/services/elevation.py
+++ terralens/services/elevation.py
@@ -1,1 +1,66 @@
+"""
+TerraLens Elevation Service
+"""
 
+
+import requests
+
+from config import Config
+
+
+
+def get_elevation(latitude, longitude):
+
+
+    payload = {
+
+
+        "locations":
+
+        [
+
+            {
+
+                "latitude":
+                latitude,
+
+
+                "longitude":
+                longitude
+
+            }
+
+        ]
+
+    }
+
+
+
+    response = requests.post(
+
+        Config.ELEVATION_URL,
+
+        json=payload
+
+    )
+
+
+
+    data = response.json()
+
+
+
+    if "results" not in data:
+
+        return None
+
+
+
+    return {
+
+
+        "elevation":
+
+        data["results"][0]["elevation"]
+
+    }
